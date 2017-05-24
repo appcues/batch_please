c2534aad29ee84485a61b0dda32f39a830414bf5
pete gamache <pete@gamache.org>
2017-05-24 16:29:08 -0400

    updated mix.exs

--- mix.exs
+++ mix.exs
@@ -3,11 +3,21 @@ defmodule BatchPlease.Mixfile do
 
   def project do
     [app: :batch_please,
+     description: "A library for collecting and processing batches of data",
+     package: package(),
      version: "0.1.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
+  end
+
+  def package do
+    [
+      maintainers: ["pete gamache <pete@appcues.com>"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/appcues/batch-please"},
+    ]
   end
 
   def application do
