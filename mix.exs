122d96494a310969288795ca092fa6c00725f97d
pete gamache <pete@gamache.org>
2017-06-01 17:41:09 -0400

    0.3.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BatchPlease.Mixfile do
     [app: :batch_please,
      description: "A library for collecting and processing batches of data",
      package: package(),
-     version: "0.2.0",
+     version: "0.3.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
